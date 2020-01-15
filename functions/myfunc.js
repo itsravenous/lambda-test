5f6a38ee05617981431fa321c0948afccb12f3fc
Tom Jardine-McNamara <tom@itsravenous.com>
2020-01-15 13:35:12 +0000

    debug

--- functions/myfunc.js
+++ functions/myfunc.js
@@ -1,4 +1,4 @@
-const {data} = require('./lib/data');
+//const {data} = require('./lib/data');
 
 exports.handler = function(event, context, callback) {
   callback(null, {
--- functions/myfunc/lib/data.js
+++
@@ -1,1 +0,0 @@
-module.exports = {data: 'some data'};
